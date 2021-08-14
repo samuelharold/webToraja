797de8cc606405acecbe3fe1e17c135964a0e55e
samuel harold wiradhika <75766013+samuelharold@users.noreply.github.com>
2021-08-14 14:01:33 +0800

    menambahkan file baru
    website ukiran toraja.

---
+++ Ukiran Toraja/assets/java.js
@@ -0,0 +1,6 @@
+console.log("Welcome To My Culture");
+
+document.getElementById("showTheList").addEventListener("click", showList);
+function showList() {
+    document.getElementById("dropDown").classList.toggle("active");
+};
